b839e4df756d951da3f1d1fb94f4430f4556e698
FelixVita <marnikaff@gmail.com>
2024-07-26 09:17:52 +0200

    Add contents for runner.py and a.py

---
+++ packagename/internal_module_a/a.py
@@ -0,0 +1,17 @@
+"""This is a module for basic arithmetic operations."""
+
+def add(x, y):
+    """This function adds two numbers."""
+    return x + y
+
+def sub(x, y):
+    """This function subtracts two numbers."""
+    return x - y
+
+def mul(x, y):
+    """This function multiplies two numbers."""
+    return x * y
+
+def div(x, y):
+    """This function divides two numbers."""
+    return x / y
---
+++ packagename/runner.py
@@ -0,0 +1,10 @@
+"""
+This is the main runner script.
+It provides specific functionality to a larger library you are packaging.
+This file is sometimes alternatively named as `main.py`.
+"""
+
+from internal_module_a.a import add
+
+print(add(10, 20))  # 30
+    
